82624f3f6266078dd2587a7222e575539a004bcc
Andrei <101584932+DashiellVienna@users.noreply.github.com>
2025-01-15 19:38:28 +0100

    Coding. Finished the project. more updates maybe will follow.

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,20 +7,59 @@
 
 import UIKit
 
+enum CurrentLight {
+    case red, yellow, green
+}
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     
-    @IBOutlet var redLightView: UIView!
-    @IBOutlet var yellowLightView: UIView!
-    @IBOutlet var greenLIghtView: UIView!
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
     
     @IBOutlet var startButton: UIButton!
+    
+    private var currentLight = CurrentLight.red
+    private let lightIsOn = 1.0
+    private let lightIsOff = 0.3
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        startButton.layer.cornerRadius = 10
+        
+        redView.alpha = lightIsOff
+        yellowView.alpha = lightIsOff
+        greenView.alpha = lightIsOff
+        
     }
-
-
+    
+    override func viewWillLayoutSubviews() {
+        redView.layer.cornerRadius = redView.frame.width / 2
+        yellowView.layer.cornerRadius = redView.frame.width / 2
+        greenView.layer.cornerRadius = redView.frame.width / 2
+        
+    }
+    
+    @IBAction func startButtonPressed() {
+        if startButton.currentTitle == "Start" {
+            startButton.setTitle("Next", for: .normal)
+        }
+        
+        switch currentLight {
+        case .red:
+            greenView.alpha = lightIsOff
+            redView.alpha = lightIsOn
+            currentLight = .yellow
+        case .yellow:
+            redView.alpha = lightIsOff
+            yellowView.alpha = lightIsOn
+            currentLight = .green
+        case .green:
+            greenView.alpha = lightIsOn
+            yellowView.alpha = lightIsOff
+            currentLight = .red
+        }
+    }
+    
 }
-
